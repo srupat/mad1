90c79facc3bd2bef747935b8d19db43d3a9dc667
srupat <srujanpatwardhan@gmail.com>
2023-10-22 18:39:08 +0530

    jinja

--- week3/app.py
+++ week3/app.py
@@ -1,14 +1,18 @@
 from jinja2 import Template
 
-
-TEMPLATE = """
-Hello {{name}} !
-"""
-# jnanapith_data = {"year":1965, "awardees": }
+jnanapith_data = [{"year": 1965, "awardees":"Umesh Patwardhan", "language":"Marathi"}, {"year": 1966, "awardees":"Vedashree Patwardhan", "language":"Hindi"}, {"year": 1989, "awardees":"Srujan Patwardhan", "language":"English"}]
 
 def main():
-    template = Template(TEMPLATE)
-    print(template.render(name="Srujan"))
+    template_file = open("jnanapith.html.jinja2")
+    TEMPLATE = template_file.read()
+    template_file.close()
 
-if __name__ == "__main__":
+    template = Template(TEMPLATE)
+    content = template.render(jnanapith_data = jnanapith_data)
+
+    my_html_document_file = open("jnanapith.html", "w")
+    my_html_document_file.write(content)
+    my_html_document_file.close()
+
+if __name__ == "__main__": #checks the current scope
     main()
---
+++ week3/assn/app.py
@@ -0,0 +1,21 @@
+import sys
+import csv
+
+f = open('data.csv')
+T
+
+
+# if(sys.argv[1]=='s'):
+
+# if(sys.argv[1]=='c'):
+
+# else:
+
+
+# def main():
+#     template = Template(TEMPLATE)
+
+
+
+# if __name__ == "__main__":
+#     main()

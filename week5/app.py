88ebb689e689eb02f66b49825cf26b75ac1f6071
srupat <srujanpatwardhan@gmail.com>
2023-11-03 18:09:03 +0530

    web app using flasksqlalchemy

--- week5/app.py
+++ week5/app.py
@@ -35,6 +35,12 @@ def articles():
     articles = Article.query.all()
     return render_template("articles.html", articles = articles)
 
+@app.route("/articles_by/<user_name>", methods = ["GET", "POST"])
+def articles_by_author(user_name):
+    articles = Article.query.filter(Article.authors.any(username = user_name))
+    return render_template("articles_by_author.html", articles = articles, username = user_name)
+
+
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',

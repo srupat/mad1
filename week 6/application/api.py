00896f24da33c908dbb72676d0e6cf20f282c357
srupat <srujanpatwardhan@gmail.com>
2023-11-12 08:37:48 +0530

    working GET & POST

--- week 6/application/api.py
+++ week 6/application/api.py
@@ -1,9 +1,24 @@
-from flask_restful import Resource
+from flask_restful import Resource, fields, marshal_with
 from application.database import db
 from application.models import User
+from application.validation import NotFoundError
+from flask_restful import reqparse
+from application.validation import BusinessValidationError
+
+output_fields = {
+    "user_id" : fields.Integer,
+    "username" : fields.String,
+    "email" : fields.String
+}
+
+create_user_parser = reqparse.RequestParser()
+create_user_parser.add_argument('username')
+create_user_parser.add_argument('email')
 
 class UserAPI(Resource):
+    @marshal_with(output_fields)
     def get(self, username):
+        
         # get the username
         print('In UserAPI GET Method', username)
         # get the user from database based on username
@@ -11,14 +26,11 @@ class UserAPI(Resource):
 
         if user:
             # return a valid user json
-            return {"user_id": user.user_id, "username" : user.username, "email" : user.email}
+            return user
         else:
             # return 404 error
-            return " ", 404
+            raise NotFoundError(status_code = 404)
         # format the return json
-
-        # return
-        return {"username" : username, "action" : "GET"}
 
     def put(self, username):
         print("PUT username", username)
@@ -28,6 +40,29 @@ class UserAPI(Resource):
         print("DELETE username", username)
         return {"username" : username, "action" : "DELETE"}
 
+    @marshal_with(output_fields)
     def post(self):
-        print("POST username")
-        return {"action" : "POST"}
+        args = create_user_parser.parse_args()
+        username = args.get("username", None)
+        email = args.get("email", None)
+
+        if username is None:
+            raise BusinessValidationError(status_code = 400, error_code = "BE1001", error_message = "username is required")
+        
+        if email is None:
+            raise BusinessValidationError(status_code = 400, error_code = "BE1002", error_message = "email is required")
+        
+        if '@' in email:
+            pass
+        else:
+            raise BusinessValidationError(status_code = 400, error_code = "BE1003", error_message = "invalid email")
+        
+        user = db.session.query(User).filter((User.username == username) | (User.email == email)).first()
+
+        if user:
+            raise BusinessValidationError(status_code = 400, error_code = "BE1004", error_message = "duplicate user")
+        
+        new_user = User(username= username, email = email)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user, 201
